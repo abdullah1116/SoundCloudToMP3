a95f0635165bbd3263e41222dbd96bdef1b5f33b
a1116 <tmp.abdullah1@gmail.com>
2020-11-03 23:16:58 +0500

    http to https

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
-if ((empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") &&
-    strpos($_SERVER['HTTP_HOST'], 'localhost') === false
-) {
+if ((empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off")) {
     $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header('HTTP/1.1 301 Moved Permanently');
     header('Location: ' . $location);
