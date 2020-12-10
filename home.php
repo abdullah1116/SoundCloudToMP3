593f83f4cd4fad954c03ce7252534a263574bc92
vdograbber <vdograbber1@gmail.com>
2020-12-10 20:28:41 +0000

    gtag old updated

--- home.php
+++ home.php
@@ -4,16 +4,17 @@
 
 <head>
     <!-- meta -->
-    <title>SoundCloud To MP3</title>
-    <meta name="description" content="With Soundcloud to mp3 converter, get your favorite Soundcloud artist’s track, songs to Mp3. Download Soundcloud Playlist while playing your favourite tracks" />
-    <meta name="Keywords" content="Soundcloudtomp3, download soundcloud, soundcloud downloader, soundcloud to mp3 converter, soundcloud to mp3, download from soundcloud, soundcloud converter, soundcloud mp3, soundcloud playlist downloader, soundcloud downloader playlist, download playlist soundcloud, how to download from soundcloud" />
+    <title>SoundCloud To MP3 - SoundCloud Playlist Downloader</title>
+    <meta name="description" content="SoundCloud Playlist Downloader: With Soundcloud to mp3 converter, get your favorite Soundcloud artist’s songs & track to Mp3. The best Soundcloud playlist downloader with an option to play and download a full playlist. Step by step guide for the playlist download and selective songs." />
+    <meta name="Keywords" content="download soundcloud,soundcloud downloader,soundcloud to mp3 converter,soundcloud playlist downloader,how to download from soundcloud" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="content-language" content="en-us" />
-    <meta name="copyright" content="soundcloudtomp3.live">
     <meta name="robots" content="index, follow">
-    <meta http-equiv="content-type" content="text/html;UTF-8">
-    <meta http-equiv="cache-control" content="cache">
-    <meta http-equiv="content-language" content="en">
+    <link rel="canonical" href="https://soundcloudtomp3.live/" />
+    <link rel="alternate" href="https://soundcloudtomp3.live/" hreflang="en-us" />
+
+
+
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://soundcloudtomp3.live/">
@@ -26,6 +27,7 @@
     <meta property="twitter:title" content="SoundCloud To MP3 - SoundCloud Playlist Downloader">
     <meta property="twitter:description" content="SoundCloud Playlist Downloader: With Soundcloud to mp3 converter, get your favorite Soundcloud artist’s songs & track to Mp3. The best Soundcloud playlist downloader with an option to play and download a full playlist. Step by step guide for the playlist download and selective songs.">
     <meta property="twitter:image" content="https://soundcloudtomp3.live/assets/soundcloud-converter.png">
+
     <script type="application/ld+json">
         {
             "@context": "http://schema.org/",
@@ -62,6 +64,183 @@
         }
     </script>
 
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "FAQPage",
+            "mainEntity": [{
+                "@type": "Question",
+                "name": "What is the best SoundCloud downloader?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "Among the many options available on the internet SoundCloudtoMP3.live is the best option. Because it allows not only a single-track download but also a playlist, you can select particular songs from the playlist to download. No subscription, no registration hence for 24 hours and seven days downloading place for free."
+                }
+            }, {
+                "@type": "Question",
+                "name": "How can I listen to SoundCloud offline without Soundcloud?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "There is no other blessing for a music addict to have a song to listen to offline. Therefore, here you can download the track present on Soundcloud and listen to it even offline. Do not worry; it is free ; you have to decide and download."
+                }
+            }, {
+                "@type": "Question",
+                "name": "How to save downloaded tracks in a different folder?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "SoundCloud files are stored in the default folder that you have already selected. However, if you want to change the storage location, you can move the files or change the destination folder during the download. I recommend you to change the destination folder as it is less time-consuming."
+                }
+            }, {
+                "@type": "Question",
+                "name": "How do I download an entire SoundCloud playlist?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "It is effortless to download from SoundCloud online. There is a search bar at the top of the page; it allows you to paste the link. Choose the playlist and enter the link here; the playlist will appear in less than 5 seconds. You can either choose the whole playlist or some songs to download."
+                }
+            }, {
+                "@type": "Question",
+                "name": "Can I download songs from SoundCloud?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "Downloading from soundcloud.com is not similar to what we are providing. It is quite a task to download from it. However, if you download from above, you can have access to the music online and offline.
+
+                    If the channel artist has chosen the option to make the track available to download.Be advised that downloading an item from SoundCloud without permission will be a violation of the company’ s copyright laws.
+                    "
+                }
+            }, {
+                "@type": "Question",
+                "name": "Why is the MP3 format preferred to download audios?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "The primary reason for choosing the MP3 format is the reduced size of the file, which ultimately consumes less storage space. Its file size is 1/10th of the original track. However, some say that the MP3 is a bit different from the original in terms of bass quality, but most of the time, one can not differentiate between them"
+                }
+            }, {
+                "@type": "Question",
+                "name": "Is there any way to download SoundCloud audios legally?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "Sure, you can, by using SoundCloudToMp3.live. The tracks you can download from here are the ones that have the permission of the owner."
+                }
+            }, {
+                "@type": "Question",
+                "name": "Where are SoundCloud audios safe by default?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "When using Chrome or any other web page, the downloaded tracks are placed in the Download Folder. Once the audio is downloaded, you can change the location as you want. 
+                    However,
+                    if you have trouble finding the audio,
+                    you can either press\ "Ctrl + J\" or search it by writing in the search bar."
+                }
+            }, {
+                "@type": "Question",
+                "name": "Unable to download the SoundCloud audio, reason?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "First of all, I would highly recommend you to update the browser you are using. Remember, there are two buttons one plays the track and the other one download. Check out the guide here to learn How To Use It. (add anchor link)
+
+                    However,
+                    if the song doesn 't download, it is probably the owner who hasn'
+                    t been permitted to download.
+                    "
+                }
+            }, {
+                "@type": "Question",
+                "name": "Can I download SoundCloud audios on Android?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "Yes, you can! Use SoundCloudToMP3.live and get any track that you like on SoundCloud and download it easily."
+                }
+            }, {
+                "@type": "Question",
+                "name": "How to download SoundCloud audios on iPhone?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "This webpage is compatible with every browser and smartphone. Open it in your iPhone and copy the URL of your favorite songs from SoundCloud. Paste it in the search bar; now, you can listen or download it as per your choice."
+                }
+            }, {
+                "@type": "Question",
+                "name": "How to download Private SoundCloud songs?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "This webpage is best for the public as well as private accounts. Copy the URL and paste it into the search bar. Either listen or download it as you want."
+                }
+            }, {
+                "@type": "Question",
+                "name": "Does this tool save user's data, particularly the download history?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "To provide the best user experience and protect the user's identity and history, we have encrypted this page with Https. It indicates an End to End connection; therefore, we do not have access to any user's data."
+                }
+            }, {
+                "@type": "Question",
+                "name": "Is it free to download songs, or will there be any hidden charges?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "It is free to listen to or download any audio you want. There are no charges and no subscription. Therefore, a lifetime webpage to have your favorite songs at free cost."
+                }
+            }, {
+                "@type": "Question",
+                "name": "Can I listen to SoundCloud music offline?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "Yes, you can listen to it offline. At every audio, you will find a heart icon. Once you select it, the audio gets added to your favorite tracks, and you can play it offline. But lately, the users are complaining that the songs do not play offline smoothly, and also only a numbered songs can be in the offline music options. 
+
+                    Therefore,
+                    it is better to have them downloaded.
+                    "
+                }
+            }, {
+                "@type": "Question",
+                "name": "How much does SoundCloud charge?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "There are two options:
+
+                    SoundCloud Pro with $7 per month
+
+                    SoundCloud Pro Unlimited $15 per month "
+                }
+            }, {
+                "@type": "Question",
+                "name": "How can I download my songs from Soundcloud?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "Go to your account in SoundCloud and choose the track you want to download. Select the share option and copy the URL. Now come to this page and paste it in the above bar."
+                }
+
+            }, {
+
+                "@type": "Question",
+                "name": "How to make a SoundCloud account?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "Follow the easy steps below to set up your SoundCloud account  
+                    Search SoundCloud.com and select Sign - Up
+
+                    From the list of music,
+                    choose the one according to your taste.
+
+                    Enter in the bars the required knowledge and verify your account
+
+                    After the setup,
+                    you can upload as many songs as you want.
+                    "
+
+                }
+
+            }, {
+
+
+                "@type": "Question",
+                "name": "How can you unblock Soundcloud?",
+                "acceptedAnswer": {
+                    "@type": "Answer",
+                    "text": "Well while you are reading this, it means you are the platform where you don't need to unblock Soundcloud music. Search for your favorite track or Soundcloud playlist and you can listen to the song or you can download it for enjoying offline."
+                }
+
+            }]
+        }
+    </script>
+
     <script src="./js/jquery.js"></script>
 
     <!-- css -->
@@ -70,11 +249,49 @@
     <link rel="stylesheet" href="./styles/index.css?v=6" />
     <link rel="stylesheet" href="./styles/loader.css" />
     <link rel="stylesheet" href="./styles/jquery-ui.css" />
-
     <link rel="icon" href="./assets/favicon.ico" />
+
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-NTLHWK3');
+    </script>
+    <!-- End Google Tag Manager -->
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-B2HCWT0L3K"></script>
+
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-B2HCWT0L3K');
+    </script>
+
 </head>
 
 <body>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTLHWK3" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <!-- navebar -->
     <!-- <nav class="navbar navbar-dark font-weight-bold shadow def-color">
             <a class="navbar-brand" href="./">SoundCloud To MP3</a>
@@ -130,6 +347,16 @@
                 </div>
             </div>
 
+            <!-- Playlist downloader -->
+            <div class="tips-cont" id="playlist-downloader">
+                <div class="tips">
+                    <span class="tips-title">Playlist downloader</span>
+                    <div class="p-3 tip-content">
+                        <?php require_once  APP_PATH . "/content/playlist-downloader.html" ?>
+                    </div>
+                </div>
+            </div>
+
             <!-- Contact us -->
             <div class="tips-cont" id="contact-us">
                 <div class="tips">
@@ -140,15 +367,7 @@
                 </div>
             </div>
 
-            <!-- Playlist downloader -->
-            <div class="tips-cont" id="playlist-downloader">
-                <div class="tips">
-                    <span class="tips-title">Playlist downloader</span>
-                    <div class="p-3 tip-content">
-                        <?php require_once  APP_PATH . "/content/playlist-downloader.html" ?>
-                    </div>
-                </div>
-            </div>
+
             <div class="tips-cont" id="faq">
                 <div class="tips">
                     <span class="tips-title">FAQ</span>
@@ -198,8 +417,7 @@
 <script async src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script async type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5f8b58e0488054c6"></script>
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-S361HP0XG1"></script>
+
 <script>
     document.onreadystatechange = function() {
         if (document.readyState === 'complete') {
@@ -213,17 +431,13 @@
 
             $('.view-select-img').click(selectGridView);
             $('#search-input').googleSuggest();
+
+            // if (screen.width < 1000) {
+            //     $('.view-select-img').toggleClass('view-select-img-selected');
+            //     HTMLDATA.isGridView = false;
+            // }
         }
     };
-
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-
-    gtag('config', 'G-S361HP0XG1');
 </script>
 
 </html>
