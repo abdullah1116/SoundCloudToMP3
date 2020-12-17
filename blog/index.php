88196a5c5abc0219397947428e806665be83b426
vdograbber <vdograbber1@gmail.com>
2020-12-17 21:48:54 +0000

    playlist container

--- blog/index.php
+++ blog/index.php
@@ -1,142 +1,132 @@
 <!DOCTYPE html>
 <html lang="en" class="blog">
-    <?php require_once "../functions/app.php" ?>
+<?php require_once "../functions/app.php" ?>
 
-    <head>
-        <link rel="stylesheet" href="/styles/bootstrap.min.css" />
-        <link rel="stylesheet" href="/styles/index.css?v=6" />
-        <link rel="stylesheet" href="/styles/loader.css" />
+<head>
+    <link rel="stylesheet" href="/styles/bootstrap.min.css" />
+    <link rel="stylesheet" href="/styles/index.css?v=6" />
+    <link rel="stylesheet" href="/styles/loader.css" />
 
-        <link
-            href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
-            rel="stylesheet"
-        />
-        <script src="/js/jquery.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet" />
+    <script src="/js/jquery.js"></script>
 
-        <meta
-            name="description"
-            content="Download your favorite tracks to your personal storage and enjoy them even when you are offline!"
-        />
-        <meta
-            name="Keywords"
-            content="soundcloud,mp3,converter,free,online,download"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/assets/favicon.ico" />
-        <title>SoundCloud To MP3</title>
-    </head>
+    <meta name="description" content="Download your favorite tracks to your personal storage and enjoy them even when you are offline!" />
+    <meta name="Keywords" content="soundcloud,mp3,converter,free,online,download" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="/assets/favicon.ico" />
+    <title>SoundCloud To MP3</title>
+</head>
 
-    <body>
-        <!-- navebar -->
-        <?php require_once APP_PATH . "/template/nav.php" ?>
+<body>
+    <!-- navebar -->
+    <?php require_once APP_PATH . "/template/nav.php" ?>
 
-        <div class="body-wrapper">
-            <div class="container">
-                <!-- why-choose-soundcloudtomp3live -->
-                <div
-                    class="tips-cont"
-                    id="how-to-use"
-                    href="/blog/why-choose-soundcloudtomp3live"
-                >
-                    <div class="tips">
-                        <span class="tips-title">
-                            Why choose soundcloudtomp3live
-                        </span>
-                        <div class="p-3 tip-content">
-                            <p>
-                                <a href="https://soundcloud.com/">
-                                    SoundCloud.com
-                                </a>
-                                has versatile music at one place from artists
-                                around the world. However, Soundcloudtomp3.live
-                                has all the music one can even listen to
-                                offline. Ever since I came across the trouble of
-                                not listening to
-                                <strong>SoundCloud music offline</strong>
-                                , I decided to build a tool for easy access
-                                anywhere and anytime.
-                            </p>
-                            <p>
-                                This
-                                <strong>SoundCloud downloader</strong>
-                                is here to ...
-                            </p>
-                            <a href="/blog/why-choose-soundcloudtomp3live">
-                                Read more
+    <div class="body-wrapper">
+        <div class="container">
+            <!-- why-choose-soundcloudtomp3live -->
+            <div class="tips-cont" id="how-to-use" href="/blog/why-choose-soundcloudtomp3live">
+                <div class="tips">
+                    <span class="tips-title">
+                        Why choose soundcloudtomp3live
+                    </span>
+                    <div class="p-3 tip-content">
+                        <p>
+                            <a href="https://soundcloud.com/">
+                                SoundCloud.com
                             </a>
-                        </div>
+                            has versatile music at one place from artists
+                            around the world. However, Soundcloudtomp3.live
+                            has all the music one can even listen to
+                            offline. Ever since I came across the trouble of
+                            not listening to
+                            <strong>SoundCloud music offline</strong>
+                            , I decided to build a tool for easy access
+                            anywhere and anytime.
+                        </p>
+                        <p>
+                            This
+                            <strong>SoundCloud downloader</strong>
+                            is here to ...
+                        </p>
+                        <a href="/blog/why-choose-soundcloudtomp3live">
+                            Read more
+                        </a>
                     </div>
                 </div>
-                <!-- why-choose-soundcloudtomp3live END -->
-                <!-- sound-cloud-vs-spotify-much-needed-comparison -->
-                <div
-                    class="tips-cont"
-                    id="how-to-use"
-                    href="/blog/sound-cloud-vs-spotify-much-needed-comparison"
-                >
-                    <div class="tips">
-                        <span class="tips-title">
-                            SoundCloud vs. Spotify; much-needed comparison
-                        </span>
-                        <div class="p-3 tip-content">
-                            <p>
-                                These are two main music streams which are
-                                running par-to-par for years now. Undoubtedly
-                                the competitions are very high, but you should
-                                understand that both are dealing in different
-                                music markets ...
-                            </p>
-                            <a
-                                href="/blog/sound-cloud-vs-spotify-much-needed-comparison"
-                            >
-                                Read more
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <!-- sound-cloud-vs-spotify-much-needed-comparison END -->
-                <!-- sound-cloud-the-hub-of-underground-music -->
-                <div
-                    class="tips-cont"
-                    id="how-to-use"
-                    href="/blog/sound-cloud-the-hub-of-underground-music"
-                >
-                    <div class="tips">
-                        <span class="tips-title">
-                            SoundCloud; the hub of Underground Music
-                        </span>
-                        <div class="p-3 tip-content">
-                            <p>
-                                SoundCloud stepped in the music industry in
-                                2007. Technically it holds its origin in
-                                Germany. But music knows no boundaries; hence,
-                                it became everyone&#39;s favourite platform in
-                                no time.
-                            </p>
-                            <p>
-                                Reasons for SoundCloud app&#39;s high popularity
-                                ...
-                            </p>
-                            <a
-                                href="/blog/sound-cloud-the-hub-of-underground-music"
-                            >
-                                Read more
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <!-- sound-cloud-the-hub-of-underground-music END -->
             </div>
-        </div>
-        <?php require_once APP_PATH . "/template/footer.php" ?>
-    </body>
+            <!-- why-choose-soundcloudtomp3live END -->
+            <!-- sound-cloud-vs-spotify-much-needed-comparison -->
+            <div class="tips-cont" id="how-to-use" href="/blog/sound-cloud-vs-spotify-much-needed-comparison">
+                <div class="tips">
+                    <span class="tips-title">
+                        SoundCloud vs. Spotify; much-needed comparison
+                    </span>
+                    <div class="p-3 tip-content">
+                        <p>
+                            These are two main music streams which are
+                            running par-to-par for years now. Undoubtedly
+                            the competitions are very high, but you should
+                            understand that both are dealing in different
+                            music markets ...
+                        </p>
+                        <a href="/blog/sound-cloud-vs-spotify-much-needed-comparison">
+                            Read more
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- sound-cloud-vs-spotify-much-needed-comparison END -->
+            <!-- sound-cloud-the-hub-of-underground-music -->
+            <div class="tips-cont" id="how-to-use" href="/blog/sound-cloud-the-hub-of-underground-music">
+                <div class="tips">
+                    <span class="tips-title">
+                        SoundCloud; the hub of Underground Music
+                    </span>
+                    <div class="p-3 tip-content">
+                        <p>
+                            SoundCloud stepped in the music industry in
+                            2007. Technically it holds its origin in
+                            Germany. But music knows no boundaries; hence,
+                            it became everyone&#39;s favourite platform in
+                            no time.
+                        </p>
+                        <p>
+                            Reasons for SoundCloud app&#39;s high popularity
+                            ...
+                        </p>
+                        <a href="/blog/sound-cloud-the-hub-of-underground-music">
+                            Read more
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- sound-cloud-the-hub-of-underground-music END -->
+            <!-- soundcloud-playlist-downloader -->
+            <div class="tips-cont" id="how-to-use" href="/blog/soundcloud-playlist-downloader">
+                <div class="tips">
+                    <span class="tips-title">
+                        SoundCloud; the hub of Underground Music
+                    </span>
+                    <div class="p-3 tip-content">
+                        <p>
+                            <p>These are two main music streams which are running par-to-par for years now.
+                                Undoubtedly the competitions are very high, but you should understand that both
+                                are dealing in different music markets.&nbsp;</p>
 
-    <script
-        async
-        src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-        crossorigin="anonymous"
-    ></script>
-    <script async src="/js/modal.js"></script>
-    <script async src="/js/linkedDiv.js"></script>
+                            <a href="/blog/soundcloud-playlist-downloader">
+                                Read more
+                            </a>
+                    </div>
+                </div>
+            </div>
+            <!-- soundcloud-playlist-downloader END -->
+        </div>
+    </div>
+    <?php require_once APP_PATH . "/template/footer.php" ?>
+</body>
+
+<script async src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script async src="/js/modal.js"></script>
+<script async src="/js/linkedDiv.js"></script>
+
 </html>
