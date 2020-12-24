68aa8b1f0525b57d0d435c134c980173bfc83865
1116 <tmp.abdullah1@gmail.com>
2020-12-24 18:12:08 +0500

    client_id update

--- functions/soundcloud.php
+++ functions/soundcloud.php
@@ -8,7 +8,7 @@ require_once APP_PATH . '/functions/mappers.php';
 // const API_URL = "https://api.soundcloud.com/%method%&client_id=BVTnmQP4X7xo1VXiYwZTNAM9swaZthcP"; // soundcloud XXX
 const API_URL = "https://api.soundcloud.com/%method%&client_id=86b6a66bb2d863f5d64dd8a91cd8de94"; // youtube
 const GEN_API_URL = "https://www.genmp3.net/getStream.php?%method%&&apikey=cldvdosndmp320"; // genmp3
-const V2_API_URL = "https://api-v2.soundcloud.com/%method%&client_id=njlDi9nZVS8dM70mLDjJpD8PascrK3xJ"; // genmp3
+const V2_API_URL = "https://api-v2.soundcloud.com/%method%&client_id=NpVHurnc1OKS80l6zlXrEVN4VEXrbZG4"; // genmp3
 
 function getSearch()
 {
