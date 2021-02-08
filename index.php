e9fd54d71660e7fe40e4f94a3b6e916cff5b2ef4
a1116 <tmp.abdullah1@gmail.com>
2021-02-09 03:39:54 +0500

    redirect to non www

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
-if ((empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") && $_SERVER['SERVER_NAME'] != 'localhost') {
-    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+if (
+    (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") && $_SERVER['SERVER_NAME'] != 'localhost'
+    || substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.'
+) {
+
+    $HTTP_HOST = substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.'
+        ? substr($_SERVER['HTTP_HOST'], 4)
+        : $_SERVER['HTTP_HOST'];
+
+    $location = 'https://' . $HTTP_HOST . $_SERVER['REQUEST_URI'];
+
     header('HTTP/1.1 301 Moved Permanently');
     header('Location: ' . $location);
 } else {
