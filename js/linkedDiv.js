571215127928b653cd72e53067365297a2f1646c
a1116 <tmp.abdullah1@gmail.com>
2021-02-10 05:02:15 +0500

    speed optimization

--- js/linkedDiv.js
+++ js/linkedDiv.js
@@ -1,14 +1,16 @@
-const linkedDiv = () => {
-    let allEle = document.getElementsByTagName('div');
-    for (var i = 0, elLength = allEle.length - 1; i < elLength; i++) {
-        let ele = allEle[i];
-        let href = ele.getAttribute('href');
-        if (href) {
-            ele.addEventListener('click', () => {
-                window.location = href;
-            });
-            ele.style.cursor = 'pointer';
+setInterval(() => {
+    const linkedDiv = () => {
+        let allEle = document.getElementsByTagName('div');
+        for (var i = 0, elLength = allEle.length - 1; i < elLength; i++) {
+            let ele = allEle[i];
+            let href = ele.getAttribute('href');
+            if (href) {
+                ele.addEventListener('click', () => {
+                    window.location = href;
+                });
+                ele.style.cursor = 'pointer';
+            }
         }
-    }
-};
-window.addEventListener('load', () => linkedDiv());
+    };
+    window.addEventListener('load', () => linkedDiv());
+});
