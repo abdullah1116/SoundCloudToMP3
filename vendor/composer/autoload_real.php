958044b2fb8697e55c3179287ec28efa8657237a
Abdullah Hasan <ahasan1116@gmail.com>
2021-02-24 18:59:07 +0500

    composer require thingengineer/mysqli-database-class:dev-master

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,9 +13,6 @@ class ComposerAutoloaderInit407b2d7c58de00727512bceee1bbffeb
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
@@ -50,6 +47,24 @@ class ComposerAutoloaderInit407b2d7c58de00727512bceee1bbffeb
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit407b2d7c58de00727512bceee1bbffeb::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire407b2d7c58de00727512bceee1bbffeb($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire407b2d7c58de00727512bceee1bbffeb($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
